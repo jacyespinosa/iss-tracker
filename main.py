239e6423ac7cb8d7655ba5ce59da936ac1ba436b
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-07-14 20:16:18 -0700

    Created the is_nigh() function to access data from sunrise-sunset API.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests
+from datetime import datetime
 
 
-MY_LAT = 37.338207
-MY_LONG = -121.886330
+MY_LAT = "ENTER YOUR LOCATION'S LATITUDE"
+MY_LONG = "ENTER YOUR LOCATION'S LONGITUDE"
 
-MY_EMAIL = "YOUR EMAIL"
-MY_PASSWORD = "YOUR PASSWORD"
-RECIPIENTS = ["ENTER RECIPIENT'S EMAIL"]
+MY_EMAIL = "ENTER EMAIL ADDRESS"
+MY_PASSWORD = "ENTER PASSWORD"
+RECIPIENTS = ["ENTER THE RECIPIENT'S EMAIL ADDRESS"]
+
 
 '''
 GET DATA FROM THE ISS API TO ACCESS THE ISS' POSITION AND LATITUDE AND LONGTITUDE.
@@ -25,4 +27,30 @@ def is_near_me():
     my_long_plus = MY_LONG + 5
     my_long_minus = MY_LONG - 5
     if my_lat_minus <= iss_latitude <= my_lat_plus and my_long_minus <= iss_longitude <= my_long_plus:
+        return True
+
+'''
+By using sunrise-sunset(https://sunrise-sunset.org/api) API, it requires two parameters: longitude and latitude.
+Pass in your current location's longitude and latitude to get the sunrise and sunset data. However, there is an extra
+parameter in the PARAMETERS -{ "formatted:" 0), which basically means that the data returned is in UNIX time
+(12-hr time). Moreover, we also need to access the current time in the current location. 
+We can do it by importing datetime. 
+'''
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0]) + 5
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0]) + 5
+
+    time_now = datetime.now()
+    hour_now = time_now.hour
+
+    if hour_now >= sunset or hour_now <= sunrise:
         return True
